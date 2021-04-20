233584251af70f64f7cd97d9da117e6167da1f08
KiMing <648063762@qq.com>
2021-04-20 15:31:11 +0800

    json数据相关 读写及转化

--- main.go
+++ main.go
@@ -3,11 +3,32 @@ package main
 import (
 	_ "GoGF/boot"
 	_ "GoGF/router"
+	"fmt"
+	"github.com/gogf/gf/encoding/gjson"
 	"github.com/gogf/gf/frame/g"
 )
 
 func main() {
+	//testJson()
 	s := g.Server()
 	s.Run()
+
+}
+
+func testJson() {
+	j := gjson.New(nil)
+	j.Set("code", 200)
+	j.Set("message", "success")
+	j.Set("data.name", "ztm")
+	j.Set("data.age", 20)
+	fmt.Println(j.MustToJsonString())
+
+	if e, err := gjson.DecodeToJson(j.MustToJsonString()); err != nil {
+		fmt.Println(err)
+	} else {
+		e.Set("code", 0)
+		e.Set("data.mobile", "15107152048")
+		fmt.Println(e.MustToJsonString())
+	}
 
 }
